7de7b94be437a9349f73d455e460eecfff48229b
Cameron Chandler <webstormcam@gmail.com>
2021-12-31 03:29:39 -0500

    I made the numbers change on toggle
    I did it!

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
+let toggle = document.getElementById('toggle');
+let basic = document.getElementById('basic');
+let pro = document.getElementById('pro');
+let master = document.getElementById('master');
+
+
+
 function myFunction(){
-    let toggle = document.getElementById('toggle');
+   
     if(toggle.checked === true){
         console.log('TRUE')
+       basic.innerHTML ='19.99'
+       pro.innerHTML ='24.99'
+       master.innerHTML ='39.99'
     } else{
         console.log('FALSE')
+        basic.innerHTML ='199.99'
+        pro.innerHTML ='249.99'
+        master.innerHTML = '399.99'
     }
 }
